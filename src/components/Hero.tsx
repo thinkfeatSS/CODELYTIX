8432bfafbd7ba52ea3cf594853a1d8ecd404c545
unknown <thinkfeat@gamil.com>
2023-12-16 08:00:37 +0500

    responsive

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className=' flex flex-grow h-screen md:h-[90vh] justify-between md:px-20 lg:px-0  lg:pl-12 xl:pl-24 bg-[#F2F6FB] z-50 shadow-inner shadow-black'>
 
     <div className="flex lg:flex-1 flex-col gap-8 xl:gap-16 lg:w-[60%] xl:w-[45%] justify-center ">
-      <h1 className="px-4 text-center font-bold md:text-left text-3xl leading-loose lg:text-4xl lg:leading-loose">
+      <h1 className="text-center font-bold md:text-left text-3xl leading-loose lg:text-4xl lg:leading-loose">
         <span className='bg-[#D0CBE6] rounded-full p-2'>Revolutionize</span> Your Software <span className='bg-[#D0CBE6] rounded-full p-2'>Engineering</span> Experience
       </h1>
       <h5 className="text-center md:text-left md:text-lg lg:text-xl font-medium ">
